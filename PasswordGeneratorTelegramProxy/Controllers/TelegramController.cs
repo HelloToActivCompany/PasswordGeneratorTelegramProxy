b16f64b76724ad5c339c45e2045e32abfb14a0f8
Александр Сараев < >
2017-03-20 21:53:36 +0300

    del notuse directive

--- PasswordGeneratorTelegramProxy/Controllers/TelegramController.cs
+++ PasswordGeneratorTelegramProxy/Controllers/TelegramController.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Web.Http;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Telegram.Bot.Types;
 using System.ServiceModel;
 using System.Configuration;
