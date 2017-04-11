70e94a67a1d00d7fdca05f59968e1ac4198b3ecc
Александр Сараев < >
2017-04-11 20:54:30 +0300

    add signalr

--- PasswordGeneratorTelegramProxy/Controllers/TelegramController.cs
+++ PasswordGeneratorTelegramProxy/Controllers/TelegramController.cs
@@ -8,17 +8,32 @@ using System.Collections.Specialized;
 using System.Text;
 using Newtonsoft.Json;
 using System.Web.Http.Results;
+using Microsoft.AspNet.SignalR.Client;
 
 namespace PasswordGeneratorTelegramProxy.Controllers
 {
     [Route("api/telegram")]
     public class TelegramController : ApiController
-    { 
+    {
+        public static HubConnection HubConnection { get; set; }
+        public static IHubProxy PasswordGenerator { get; set; }
+
+        TelegramController()
+        {
+            
+        }
+
         [HttpPost]
         public void GetPassword([FromBody]Update update)
         {
             var token = ConfigurationManager.AppSettings["TelegramBotToken"];
             var bot = new Telegram.Bot.TelegramBotClient(token);
+
+            HubConnection = new HubConnection("http://passwordgeneratorfacade.azurewebsites.net/");
+            var PasswordGenerator = HubConnection.CreateHubProxy("PasswordHub");
+            PasswordGenerator.On<string>("passwordReady", (password) => { bot.SendTextMessageAsync(update.Message.Chat.Id, password); });
+            //ServicePointManager.DefaultConnectionLimit = 10;
+            HubConnection.Start().Wait();
 
             if (!String.IsNullOrWhiteSpace(update.Message.Text))
             {
@@ -37,9 +52,14 @@ namespace PasswordGeneratorTelegramProxy.Controllers
                 var key = param[0];
                 var source = param[1];
 
-                var password = PostToAggregationService(key, source);
-
-                bot.SendTextMessageAsync(update.Message.Chat.Id, password);
+                //var password = PostToAggregationService(key, source);
+                var requestData = JsonConvert.SerializeObject(new
+                {
+                    Key = key,
+                    Value = source
+                });
+                WebApiApplication.PasswordGenerator.Invoke("Generate", requestData).Wait();
+                //bot.SendTextMessageAsync(update.Message.Chat.Id, password);
             }
         }
 
